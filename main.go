a5629b84713998e44e1f6fb410c892b5493d5384
dinaju <inajudaniel2050@gmail.com>
2026-04-01 13:13:12 +0100

    HexToDecimal

---
+++ main.go
@@ -0,0 +1,15 @@
+//package main
+
+//import (
+//	"fmt"
+//	"strings"
+//)
+
+//func HexToDecimal(string string) string {
+//	result := strings.ReplaceAll(str, hex, 30)
+
+//}
+
+//func main() {
+//	fmt.Println(HexToDecimal("we have 1E (hex) files added"))
+//}
